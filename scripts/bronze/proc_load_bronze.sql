d40b6419834f01ce01577b6020dcab854a583608
Tien Dat Nguyen <123169590+DattWguyn2811@users.noreply.github.com>
2025-08-18 15:16:35 +0700

    Create proc_load_bronze.sql

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,139 @@
+/*
+==============================================================
+Stored Procedure: load_bronze
+==============================================================
+Script Purpose:
+    This stored procedure loads data from raw CSV source files 
+    into the Bronze Layer of the Data Warehouse. 
+    It performs truncate-and-load operations for CRM and ERP 
+    source tables, while logging execution time for each load step.
+
+Parameters:
+    None
+
+Usage Example:
+    CALL bronze.load_bronze();
+
+Notes:
+    - All target Bronze tables will be truncated before loading.
+    - Data is ingested via the PostgreSQL COPY command.
+    - Execution times are logged for monitoring and debugging.
+==============================================================
+*/
+
+create or replace procedure bronze.load_bronze()
+language plpgsql
+as $$
+declare
+	start_time timestamp;
+	end_time timestamp;
+	duration bigint;
+	batch_start_time timestamp;
+	batch_end_time timestamp;
+begin
+	raise notice '==========================================================';
+	raise notice 'Loading Bronze Layer';
+	raise notice '==========================================================';
+
+	raise notice '----------------------------------------------------------';
+	raise notice 'Loading CRM Tables';
+	raise notice '----------------------------------------------------------';
+
+	batch_start_time = now();
+
+	start_time := now(); 
+	raise notice '>> Truncating Table: bronze.crm_cust_info';
+	truncate table bronze.crm_cust_info;
+	raise notice '>> Inserting Data Into: bronze.crm_cust_info';
+	copy bronze.crm_cust_info 
+	from '/Users/DatWguyn/Code/project/sql-data-warehouse-project/datasets/source_crm/cust_info.csv'
+	delimiter ','
+	csv header;
+	end_time := now();
+	duration := extract(epoch from (end_time - start_time));
+	raise notice '>> Load Duration: % seconds', duration;
+	raise notice '---------------------------------------';
+	
+	start_time := now(); 
+	raise notice '>> Truncating Table: bronze.crm_prd_info';
+	truncate table bronze.crm_prd_info;
+	raise notice '>> Inserting Data Into: bronze.crm_prd_info';
+	copy bronze.crm_prd_info 
+	from '/Users/DatWguyn/Code/project/sql-data-warehouse-project/datasets/source_crm/prd_info.csv'
+	delimiter ','
+	csv header;
+	end_time := now();
+	duration := extract(epoch from (end_time - start_time));
+	raise notice '>> Load Duration: % seconds', duration;
+	raise notice '---------------------------------------';
+
+	start_time := now();
+	raise notice '>> Truncating Table: bronze.crm_sales_details';
+	truncate table bronze.crm_sales_details;
+	raise notice '>> Inserting Data Into: bronze.crm_sales_details';
+	copy bronze.crm_sales_details
+	from '/Users/DatWguyn/Code/project/sql-data-warehouse-project/datasets/source_crm/sales_details.csv'
+	delimiter ','
+	csv header;
+	end_time := now();
+	duration := extract(epoch from (end_time - start_time));
+	raise notice '>> Load Duration: % seconds', duration;
+
+	raise notice '----------------------------------------------------------';
+	raise notice 'Loading ERP Tables';
+	raise notice '----------------------------------------------------------';
+
+	start_time := now();
+	raise notice '>> Truncating Table: bronze.erp_cust_az12';
+	truncate table bronze.erp_cust_az12;
+	raise notice '>> Inserting Data Into: bronze.erp_cust_az12';
+	copy bronze.erp_cust_az12
+	from '/Users/DatWguyn/Code/project/sql-data-warehouse-project/datasets/source_erp/CUST_AZ12.csv'
+	delimiter ','
+	csv header;
+	end_time := now();
+	duration := extract(epoch from (end_time - start_time));
+	raise notice '>> Load Duration: % seconds', duration;
+	raise notice '---------------------------------------';
+
+	start_time := now();
+	raise notice '>> Truncating Table: bronze.erp_loc_a101';
+	truncate table bronze.erp_loc_a101;
+	raise notice '>> Inserting Data Into: bronze.erp_loc_a101'; 
+	copy bronze.erp_loc_a101
+	from '/Users/DatWguyn/Code/project/sql-data-warehouse-project/datasets/source_erp/LOC_A101.csv'
+	delimiter ','
+	csv header;
+	end_time := now();
+	duration := extract(epoch from (end_time - start_time));
+	raise notice '>> Load Duration: % seconds', duration;
+	raise notice '---------------------------------------';
+
+	start_time := now();
+	raise notice '>> Truncating Table: bronze.erp_px_cat_g1v2';
+	truncate table bronze.erp_px_cat_g1v2;
+	raise notice '>> Inserting Data Into: bronze.erp_px_cat_g1v2'; 
+	copy bronze.erp_px_cat_g1v2
+	from '/Users/DatWguyn/Code/project/sql-data-warehouse-project/datasets/source_erp/PX_CAT_G1V2.csv'
+	delimiter ','
+	csv header;
+	end_time := now();
+	duration := extract(epoch from (end_time - start_time));
+	raise notice '>> Load Duration: % seconds', duration;
+
+	batch_end_time := now();
+	duration := extract(epoch from (batch_end_time - batch_start_time));
+	raise notice '==========================================================';	
+	raise notice 'Loading Bronze Layer is Completed!';
+	raise notice '>> Total Load Duration: % seconds', duration;
+	raise notice '==========================================================';
+exception	
+	when others then 
+		raise notice '==========================================================';
+		raise notice 'Error occured during loading Bronze Layer';
+		raise notice '>> Error Message: %', sqlerrm;
+		raise notice '>> Error Code: %', sqlstate;
+		raise exception '-> Failed to load Bronze Layer';
+		raise notice '==========================================================';
+end;
+$$;
